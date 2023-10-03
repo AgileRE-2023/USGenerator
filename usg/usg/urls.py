84391bc19b9743ea6d9cac8355bb2706d2c8b8d9
Darfito <darfito.danurdoro-2021@fst.unair.ac.id>
2023-10-03 23:06:16 +0700

    setting route css and templates
    Co-authored-by: Reyhan Eldwin Maulana <TetRey@users.noreply.github.com>

--- usg/usg/urls.py
+++ usg/usg/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib import client
+# from django.contrib import client
 from django.urls import path, include
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('admin/', include('admin.urls')),
-    path('client/', include('client.urls')),
+    path('admin_site/', include('admin_site.urls')),
 
 ]
