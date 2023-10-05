7f46444dad8e03b4099baaffc5bb9dfa29c9cb26
TetRey <reyhaneldwin31@gmail.com>
2023-10-05 20:11:33 +0700

    admin project done

--- usg/admin_site/urls.py
+++ usg/admin_site/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'admin_site'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('projectview', views.projectView, name='projectview')
 ]
--- usg/admin_site/views.py
+++ usg/admin_site/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'base.html')
+def projectView(request):
+    return render(request, 'project-view.html')
