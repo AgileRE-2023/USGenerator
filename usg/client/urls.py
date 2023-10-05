d45d767220bcfc1db806e7cd9b3f0a6d854e077c
safranatha <safranatha@gmail.com>
2023-10-05 16:23:45 +0700

    sign in done

--- usg/client/urls.py
+++ usg/client/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
 from . import views
-
+app_name = 'client'
 urlpatterns = [
-    path("", views.index, name="index"),
+    # path("", views.index, name="index"),
+    # path("sign_in", views.signIn, name="index")/,
+    path('base',views.base, name='index'),
+    path('baseSignIn',views.baseSignIn, name='index'),
+    path('signin',views.SignIn, name='index'),
 ]
--- usg/client/views.py
+++ usg/client/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def base(request):
+    return render(request,'base.html')
+def baseSignIn(request):
+    return render(request,'base_signin.html')
+def SignIn(request):
+    return render(request,'signin-reg/signin.html')
