db165e4d378c059e03273e45378a3cd5b4dd256c
kecoaktempur <triwan98@gmail.com>
2023-10-06 15:02:25 +0700

    fix path

--- usg/admin_site/urls.py
+++ usg/admin_site/urls.py
@@ -7,9 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('user', views.user, name='user'),
     path('projectview', views.projectView, name='projectview'),
-    path('dashboard', views.dashboard, name='dashboard'),
-    url(r'^$', views.index, name='index'),
-    url(r'^user', views.user, name='user'),
-    url(r'^dashboard', views.dashboard, name='dashboard'),
-    url(r'^projectview', views.projectView, name='projectview'),
+    path('dashboard', views.dashboard, name='dashboard')
 ]
