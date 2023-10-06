3cb13821b16ea84779d8fc88d758f778981a4abe
kecoaktempur <triwan98@gmail.com>
2023-10-06 14:40:37 +0700

    cobak urls

--- usg/usg/urls.py
+++ usg/usg/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # from django.contrib import admin
+from django.contrib import admin
 # from django.contrib import client
 from django.urls import path, include
 
@@ -22,5 +23,6 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
     # path('admin/', include('admin.urls')),
     path('client/', include('client.urls')),
+    path('admin_site/', include('admin_site.urls')),
 
 ]
