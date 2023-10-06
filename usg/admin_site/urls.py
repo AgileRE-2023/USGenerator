64d95af3ca5c133f8bb1ad800f55def88060b092
kecoaktempur <triwan98@gmail.com>
2023-10-06 08:43:14 +0700

    href patch fix

--- usg/admin_site/urls.py
+++ usg/admin_site/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from django.urls import re_path as url
 from . import views
 
 app_name = 'admin_site'
@@ -8,4 +8,8 @@ urlpatterns = [
     path('user', views.user, name='user'),
     path('projectview', views.projectView, name='projectview'),
     path('dashboard', views.dashboard, name='dashboard'),
+    url(r'^$', views.index, name='index'),
+    url(r'^user', views.user, name='user'),
+    url(r'^dashboard', views.dashboard, name='dashboard'),
+    url(r'^projectview', views.projectView, name='projectview'),
 ]
