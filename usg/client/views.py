e1a0a6fde472173c71ac1ada4a4c6aa00e8f211b
safranatha <safranatha@gmail.com>
2023-12-22 11:31:14 +0700

    project complished done

--- usg/client/views.py
+++ usg/client/views.py
@@ -749,7 +749,16 @@ def userProfile(request):
     user_auth = auth.current_user['localId']
     users_value = db.child(f'users/{user_auth}').get()
     # user.fromJson(users_by_name)
-    return render(request, 'user_profile/user-profile.html', {'user': users_value.val()})
+    # get the id of user stories, still dictionary need to convert to list
+    users_stories_title = db.child(
+        f'users/{user_auth}/userstories').shallow().get()
+    if users_stories_title.val() is None:
+        project_complished=0
+        return render(request, 'user_profile/user-profile.html', {'user': users_value.val(),'project_complished':project_complished})
+    # convert to list
+    arr_users_stories_title = list(users_stories_title.val())
+    project_complished=len(arr_users_stories_title)
+    return render(request, 'user_profile/user-profile.html', {'user': users_value.val(),'project_complished':project_complished})
 
 
 def editProfile(request):
