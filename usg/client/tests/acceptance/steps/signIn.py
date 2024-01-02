6f1c6e87587bcbfed583524ded26ad584c12c39b
Darfito <darfito.danurdoro-2021@fst.unair.ac.id>
2024-01-02 21:59:15 +0700

    fixing bdd sign in

--- usg/client/tests/acceptance/steps/signIn.py
+++ usg/client/tests/acceptance/steps/signIn.py
@@ -9,23 +9,23 @@ def step_impl(context):
     context.client = Client()
     context.browser = webdriver.Chrome()
 
-@when("i fill in username field with admin-chris")
+@when("i fill in email field with usertesting461@gmail.com")
 def step_impl(context):
-    context.browser.get('http://127.0.0.1:8000/accounts/signin') 
+    context.browser.get('http://127.0.0.1:8000/signin') 
 
     # Example: Use Selenium to fill in form fields
     username_input = context.browser.find_element(By.ID, 'email')
     username_input.send_keys('usertesting461@gmail.com')
 
 
-@when("i fill in password field with adelinejulia")
+@when("i fill in password field with UserTesting123")
 def step_impl(context):
     password_input = context.browser.find_element(By.ID, 'password')
     password_input.send_keys('UserTesting123')
     
-@when("i fill in password field with jokowowkacaw")
+@when("i fill in password field with UserTesting12")
 def step_impl(context):
-    context.browser.get('http://127.0.0.1:8000/accounts/signin')  # Update with your Django development server URL
+    context.browser.get('http://127.0.0.1:8000/signin')  # Update with your Django development server URL
 
     # Example: Use Selenium to fill in form fields
     password_input = context.browser.find_element(By.ID, 'password')
